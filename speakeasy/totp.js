88138f4f6cb0d348e447704cafb5a84d296e83ab
annie guo <annie.guo.2011@gmail.com>
2014-07-02 03:09:15 +0000

    console.log and util.inspect speakeasy.totp

--- speakeasy/totp.js
+++ speakeasy/totp.js
@@ -6,7 +6,25 @@ process.on('uncaughtException', function(err){console.log('EXIT uncaughtExceptio
 console.log("===Start")
 
 speakeasy=require('speakeasy')
-a=speakeasy.totp({key: 'secret'});
-console.dir(a)
+assert = require('assert')
+util = require('util')
+
+console.dir(speakeasy.totp)
+console.log(util.inspect(speakeasy, { showHidden: true, depth: 0 }))
+console.log(util.inspect(speakeasy.totp, { showHidden: true, depth: null }))
+console.dir(assert.equal)
+console.log(util.inspect(assert, { showHidden: true, depth: 0 }))
+console.log(util.inspect(assert.equal, { showHidden: true, depth: null }))
+
+a1=speakeasy.totp({key: 'secret'});
+a2=speakeasy.totp({key: 'secret'});
+console.log("a1 <%s>, a2 <%s>",a1, a2)
+assert.equal(a1,a2)
+
+b1=speakeasy.totp({key: 'secret', step: 60});
+b2=speakeasy.totp({key: 'secret', step: 60});
+console.log("b1 <%s>, b2 <%s>",b1, b2)
+assert.equal(b1,b2)
+
 
 console.log("===Completed successfully!")
