227db4ee3f09238abcd5077b1a4e9eee4a0a6c66
annie guo <annie.guo.2011@gmail.com>
2014-07-03 18:55:27 +0000

    added test cases for totp in node

--- speakeasy/totp.js
+++ speakeasy/totp.js
@@ -28,17 +28,56 @@ b2=speakeasy.totp({key: 'secret', step: 60});
 console.log("b1 <%s>, b2 <%s>",b1, b2)
 assert.equal(b1,b2)
 
-
+d='\n1. Test normal operation with key = \'12345678901234567890\' at time = 59'
+console.log(d)
 c=speakeasy.totp({key: '12345678901234567890', time: 59});
 console.log("c <%s>",c)
 assert.equal(c, '287082');
 
+d='\n2. Test a different time normal operation with key = \'12345678901234567890\' at time = 1111111109'
+console.log(d)
 c=speakeasy.totp({key: '12345678901234567890', time: 1111111109});
 console.log("c <%s>",c)
 assert.equal(c, '081804');
 
+d='\n3. Test length parameter with key = \'12345678901234567890\' at time = 1111111109 and length = 8'
+console.log(d)
 c=speakeasy.totp({key: '12345678901234567890', time: 1111111109, length: 8})
 console.log("c <%s>",c)
 assert.equal(c, '07081804');
 
+d='\n4. Test hexadecimal encoding with key = \'3132333435363738393031323334353637383930\' as hexadecimal at time 1111111109'
+console.log(d)
+c=speakeasy.totp({key: '3132333435363738393031323334353637383930', encoding: 'hex', time: 1111111109});
+console.log("c <%s>",c)
+assert.equal(c, '081804');
+
+d='\n5. Test base32 encoding with key = \'GEZDGNBVGY3TQOJQGEZDGNBVGY3TQOJQ\' as base32 at time 1111111109'
+console.log(d)
+c=speakeasy.totp({key: 'GEZDGNBVGY3TQOJQGEZDGNBVGY3TQOJQ', encoding: 'base32', time: 1111111109});
+console.log("c <%s>",c)
+assert.equal(c, '081804');
+
+d='\n6. Test a custom step with key = \'12345678901234567890\' at time = 1111111109 with step = 60'
+console.log(d)
+c=speakeasy.totp({key: '12345678901234567890', time: 1111111109, step: 60});
+console.log("c <%s>",c)
+assert.equal(c, '360094');
+
+
+d='\n7. Test initial time with key = \'12345678901234567890\' at time = 1111111109 and initial time = 1111111100'
+console.log(d)
+c=speakeasy.totp({key: '12345678901234567890', time: 1111111109, initial_time: 1111111100});
+console.log("c <%s>",c)
+assert.equal(c, '755224');
+
 console.log("===Completed successfully!")
+
+/**
+ref
+1) node, 
+https://github.com/markbao/speakeasy/blob/master/test/test_totp.js
+
+2) python
+https://pypi.python.org/pypi/onetimepass/0.1.2
+**/
