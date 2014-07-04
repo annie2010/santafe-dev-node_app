143d1afe5f1c3250d306586bf72c40b1e326b78e
annie guo <annie.guo.2011@gmail.com>
2014-07-04 15:26:48 +0000

    cleanup

--- speakeasy/base32.py
+++ speakeasy/base32.py
@@ -9,23 +9,24 @@ k3='Inspiration for Nemo sprang from experie' # sub
 
 from otpauth import OtpAuth
 def otpauth_totp(f_key):
-  print "\n1.b test totp"
+  print "\ntotp per raw secret"
   auth = OtpAuth(f_key) # default step=30
   code = auth.totp()
   assert auth.valid_totp(code)
   print "secret <%s> code <%s>" % (auth.secret, code)
   #print dir(auth) 
-  print "completed successfully"
 
 
 s1 = base64.b32encode(k1)
-print s1
 otpauth_totp(k1)
+print s1
 
 s2 = base64.b32encode(k2)
-print s2
 otpauth_totp(k2)
+print s2
 
 s3 = base64.b32encode(k3)
-print s3
 otpauth_totp(k3)
+print s3
+
+print "\n\ncompleted successfully"
